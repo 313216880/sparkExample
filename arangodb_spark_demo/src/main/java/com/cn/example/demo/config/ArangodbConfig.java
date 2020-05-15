c259a40bd5d92aec3cdadff1fa5eb6704f41065b
liuhao <313216880@qq.com>
2020-05-15 19:03:40 +0800

    初始化

---
+++ arangodb_spark_demo/src/main/java/com/cn/example/demo/config/ArangodbConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Company: 
+ * Copyright (c) 2012-2032 
+ * All Rights Reserved.
+ */
+package com.cn.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ArangodbConfig {
+
+}
